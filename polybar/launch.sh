576c7dd0979c6d8b80eb94bd3a54a4a3a003221b
HotMonkeyWings <devsony52@gmail.com>
2022-12-26 22:24:50 +0530

    :hammer: New laptop changes

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,12 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 
-#polybar top &
-#polybar top_external &
 polybar --config=~/.config/polybar/config-main.ini main &
-
-
-polybar --config=~/.config/polybar/config-side.ini main &
 
 #secondary=$(xrandr -q | grep 'DP-0')
 #if [[$secondary = *connnected* ]]; then
